8c56ab131c3da3d929a61303ecef56b8ed6d020b
Thiago Barroso <memoriathiago@gmail.com>
2025-05-01 11:15:57 -0300

    entidade usuário

---
+++ src/main/java/com/thiago/barroso/estacionamento/entity/Usuario.java
@@ -0,0 +1,158 @@
+package com.thiago.barroso.estacionamento.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private Long id;
+	
+	@Column(name = "username", nullable = false, length = 100)
+	private String username;
+	
+	@Column(name = "password", nullable = false, length = 200)
+	private String password;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "role", nullable = false, length = 25)
+	private Role role;
+	
+	@Column(name = "data_criacao")
+	private LocalDateTime dataCriacao;
+	
+	@Column(name = "data_modificacao")
+	private LocalDateTime dataModificacao;
+	
+	@Column(name = "criado_por")
+	private String criadoPor;
+	
+	@Column(name = "modificado_por")
+	private String modificadoPor;
+	
+	public enum Role{
+		ROLE_ADMIN, ROLE_CLIENTE
+	}
+	
+	public Usuario() {
+		super();
+	}
+
+	public Usuario(Long id, String username, String password, Role role, LocalDateTime dataCriacao,
+			LocalDateTime dataModificacao, String criadoPor, String modificadoPor) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.role = role;
+		this.dataCriacao = dataCriacao;
+		this.dataModificacao = dataModificacao;
+		this.criadoPor = criadoPor;
+		this.modificadoPor = modificadoPor;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(LocalDateTime dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	public LocalDateTime getDaraModificacao() {
+		return dataModificacao;
+	}
+
+	public void setDataModificacao(LocalDateTime dataModificacao) {
+		this.dataModificacao = dataModificacao;
+	}
+
+	public String getCriadoPor() {
+		return criadoPor;
+	}
+
+	public void setCriadoPor(String criadoPor) {
+		this.criadoPor = criadoPor;
+	}
+
+	public String getModificadoPor() {
+		return modificadoPor;
+	}
+
+	public void setModificadoPor(String modificadoPor) {
+		this.modificadoPor = modificadoPor;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", username=" + username + ", password=" + password + ", role=" + role
+				+ ", dataCriacao=" + dataCriacao + ", dataModificacao=" + dataModificacao + ", criadoPor=" + criadoPor
+				+ ", modificadoPor=" + modificadoPor + "]";
+	}
+	
+}
