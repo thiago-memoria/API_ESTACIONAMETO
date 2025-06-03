a415915f17d3cef0e8dd941d935b736a73502588
Thiago Barroso <memoriathiago@gmail.com>
2025-06-02 23:44:06 -0300

    Mapeamento da controller de autenticação

--- src/main/java/com/thiago/barroso/estacionamento/web/controller/AutenticacaoController.java
+++ src/main/java/com/thiago/barroso/estacionamento/web/controller/AutenticacaoController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +30,7 @@ public class AutenticacaoController {
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	
+	@PostMapping("/auth")
 	public ResponseEntity<?> autenticar(@RequestBody @Valid UsuarioLoginDto dto, HttpServletRequest request){
 		 try {
 			 UsernamePasswordAuthenticationToken authenticationToken = 
